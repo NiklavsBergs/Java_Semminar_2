bf60d3a84bda206d35e191a013e93d947fd51dfe
Niklāvs Bergs <s21bergnikl@vea.lv>
2023-03-03 14:34:03 +0200

    added regex

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,41 +1,50 @@
 package model;
 
 public class Student {
-	private long st_id;
+	private long id;
 	private String name;
 	private String surname;
 	private Faculty faculty;
-	private String persKods;
+	private String personCode;
 	private static long idCounter = 10000;
 	
 	public Student() {
-		st_id = idCounter;
+		id = idCounter;
 		name = "Vards";
 		surname = "Uzvards";
+		personCode = "000000-00000";
 		idCounter++;
 	}
 	
-	public Student(long p_idIn, String nameIn, String surnameIn) {
-		st_id = idCounter;
+	public Student(long p_idIn, String nameIn, String surnameIn, String personCodeIn) {
+		id = idCounter;
 		name = nameIn;
 		surname = surnameIn;
+		personCode = personCodeIn;
 		idCounter++;
 	}
 
 	public long getSt_id() {
-		return st_id;
+		return id;
 	}
 
-	public void setSt_id(long st_id) {
-		this.st_id = st_id;
+	public void setSt_id(long id) {
+		this.id = id;
 	}
 
 	public String getName() {
 		return name;
 	}
 
+	//Regex [A-ZĒŪĪĻĶĢŠĀČŅŽ]{1}[a-zēūīļķģšāčņž]+[ ]?([A-ZĒŪĪĻĶĢŠĀČŅŽ]{1}[a-zēūīļķģšāčņž]+)?
 	public void setName(String name) {
-		this.name = name;
+		if(name.matches("[A-ZĒŪĪĻĶĢŠĀČŅŽ]{1}[a-zēūīļķģšāčņž]+[ ]?([A-ZĒŪĪĻĶĢŠĀČŅŽ]{1}[a-zēūīļķģšāčņž]+)?")) {
+			this.name = name;
+		}
+		else {
+			this.name = "Unknown";
+		}
+		
 	}
 
 	public String getSurname() {
